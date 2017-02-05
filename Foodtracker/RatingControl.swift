7ac7a9d6c4fe79126675edfbb484e8d1eb86acda
kN3TT3R <knetter@ptr-916yrbl8jp4st6yb1qx.18120a2.ip6.access.telenet.be>
2017-02-05 17:33:34 +0100

    Added RatingControl -> custom view subclass of UIStackView

---
+++ Foodtracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  Foodtracker
+//
+//  Created by Kenneth Debruyn on 5/02/17.
+//  Copyright © 2017 kN3TT3R. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
